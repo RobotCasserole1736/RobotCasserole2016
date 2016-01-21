843b26a7552d45abe3ce45959c866ebf0faa7f3f
Mohammed Alashqar <malashqar360.quest@gmail.com>
2016-01-20 20:53:14 -0600

    Added VisionTracker

---
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/VisionTracker.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team1736.robot;
+import java.io.IOException;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class VisionTracker {
+	public void readTargets(){
+		
+	}
+
+	public void beginTracking(){
+		
+	}
+	
+	public void trackingIiterate(){
+		
+	}
+
+}
