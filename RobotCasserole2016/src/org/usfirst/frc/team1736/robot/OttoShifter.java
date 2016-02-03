a6f6caad3a80881b3ec5d95a1e15c2e90adb13a5
Alex Stevenson <ruski550@gmail.com>
2016-02-02 20:29:24 -0600

    Started OttoShifter

---
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/OttoShifter.java
@@ -0,0 +1,69 @@
+package org.usfirst.frc.team1736.robot;
+
+public class OttoShifter {
+	
+	public static final double VAL_DEBOUNCE_THRESH = 1000;
+	public static final int VAL_DEBOUNCE_TIME = 1000;
+	public static final double WHEEL_ACCEL_DEBOUNCE_THRESH = 1000;
+	public static final int WHEEL_ACCEL_DEBOUNCE_TIME = 1000;
+	public static final double VERT_ACCEL_DEBOUNCE_THRESH = 1000;
+	public static final int VERT_ACCEL_DEBOUNCE_TIME = 1000;
+	public static final double CURRENT_DEBOUNCE_THRESH = 1000;
+	public static final int CURRENT_DEBOUNCE_TIME = 1000;
+
+
+
+	
+	public boolean gear; //True is high and false is low
+	public DaBouncer VelDebounce;
+	public DaBouncer WheelAccelDebounce;
+	public DaBouncer VertAccelDebounce;
+	public DaBouncer CurrentDebounce;
+	
+	
+	OttoShifter(){
+		VelDebounce = new DaBouncer();
+		WheelAccelDebounce = new DaBouncer();
+		VertAccelDebounce = new DaBouncer();
+		CurrentDebounce = new DaBouncer();
+		
+		gear = true;
+		
+		VelDebounce.threshold = VAL_DEBOUNCE_THRESH;
+		VelDebounce.dbnc = VAL_DEBOUNCE_TIME;
+		WheelAccelDebounce.threshold = WHEEL_ACCEL_DEBOUNCE_THRESH;
+		WheelAccelDebounce.dbnc = WHEEL_ACCEL_DEBOUNCE_TIME;
+		VertAccelDebounce.threshold  = VERT_ACCEL_DEBOUNCE_THRESH;
+		VertAccelDebounce.dbnc = VERT_ACCEL_DEBOUNCE_TIME; 
+		CurrentDebounce.threshold = CURRENT_DEBOUNCE_THRESH;
+		CurrentDebounce.dbnc = CURRENT_DEBOUNCE_TIME; 
+	}
+	 public void OttoShifterPeriodic(
+			 double VelocityValue,
+			 double WheelAceelValue,
+			 double VerAccelValue,
+			 double CurrentValue,
+			 boolean DriverUpshiftCmd,
+			 boolean DriverDownshiftCmd){
+		/* switch (gear){
+		 case true:
+			 
+			 break;
+			 
+		 case false;
+		 
+		 break;
+	Didn't work- Riperino in Peperino
+			*/ 
+		 
+		 if (gear == true){
+			 
+		 }
+		 else{ //gear == false
+		 
+		 }
+		 }
+		 
+	 }
+	}
+	
