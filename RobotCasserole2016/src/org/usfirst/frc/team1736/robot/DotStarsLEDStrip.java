aceb30f8d1f70105afee74b8da9caf11bb484876
Chris Gerth <chrisgerth010592@gmail.com>
2016-02-07 19:28:09 -0600

    lots of bug fixes for LED strip api

--- RobotCasserole2016/src/org/usfirst/frc/team1736/robot/DotStarsLEDStrip.java
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/DotStarsLEDStrip.java
@@ -4,6 +4,7 @@
 package org.usfirst.frc.team1736.robot;
 
 import java.util.Arrays;
+import java.util.TimerTask;
 
 import edu.wpi.first.wpilibj.SPI;
 
@@ -14,22 +15,32 @@ import edu.wpi.first.wpilibj.SPI;
 public class DotStarsLEDStrip {
 	//Datasheet - https://www.adafruit.com/datasheets/APA102.pdf
 	
+	//Thread stuff
+	// Thread to run the periodic update function on 
+	java.util.Timer timerThread; 
+
+	//period between periodic function calls
+	//in milliseconds
+	double m_update_period_ms = 100; 
+	
 	//Constant values for fixed things in the serial data stream
 	static byte[] startFrame = {0x00, 0x00, 0x00, 0x00};
-	static byte[] endFrame = {(byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF}; //Java is stupid and doesn't like unsigned things. 
-	static byte globalBrighness = (byte)0xFF;
+	static byte[] endFrame = {(byte)(255), (byte)(255), (byte)(255), (byte)(255)}; //Java is stupid and doesn't like unsigned things. 
 	
 	//Offsets within the stream
+	static int globalOffset = 0;
+	static int blueOffset = 2; //Note that green/blue are inverted from wh at the dtatsheet says they should be
 	static int greenOffset = 1;
-	static int blueOffset = 2;
 	static int redOffset = 3;	
 	static int bytesPerLED = 4;
 	static int led0Offset = startFrame.length;
 	static int endFrameOffset; //dependent on number of LED's
+	
+	int num_leds;
 
 	//SPI coms object from FRC
 	SPI spi;
-	static final int SPI_CLK_RATE = 512000; //Total guess at max clock rate - 512KHz is called out in an example in the datasheet, so I used that
+	static final int SPI_CLK_RATE = 13000000; //Total guess at max clock rate - 512KHz is called out in an example in the datasheet, so I used that
 	
 	//Color Buffer - all bytes to send out from the 
 	byte[] ledBuffer;
@@ -47,6 +58,7 @@ public class DotStarsLEDStrip {
 	DotStarsLEDStrip(int numLEDs){
 		//Number of bytes in color buffer needed - each LED has 4 bytes (1 brightness, then 1 for RGB each),
 		// plus the start and end frame.
+		num_leds = numLEDs;
 		int num_bytes_for_strip = 4*numLEDs + startFrame.length + endFrame.length;
 		endFrameOffset = 4*numLEDs + startFrame.length;
 		
@@ -67,7 +79,10 @@ public class DotStarsLEDStrip {
 		spi.setMSBFirst();
 		spi.setClockActiveLow();
 		spi.setClockRate(SPI_CLK_RATE); 
-		spi.setSampleDataOnRising();
+		spi.setSampleDataOnFalling();
+		
+        timerThread = new java.util.Timer();
+        timerThread.schedule(new DotStarsTask(this), (long) (m_update_period_ms), (long) (m_update_period_ms));
 		
 	}
 	
@@ -81,15 +96,26 @@ public class DotStarsLEDStrip {
 		
 		//If we need to write something, attempt to put it on the SPI port
 		if(newBuffer){
-			ret_val =  spi.write(ledBuffer, ledBuffer.length);
+			//Make local copy of ledBuffer to help make color changing clean
+			byte[] temp_ledBuff = Arrays.copyOfRange(ledBuffer, 0, ledBuffer.length);
+			
+			//Chunk the TX'es into smaller size, since it looks like
+			//we can only TX 128 bytes at a time
+			for(int offset = 0; offset < temp_ledBuff.length; offset = offset+128){
+				int start_index = offset;
+				int end_index = Math.min(offset+128, temp_ledBuff.length);
+				int size = end_index - start_index;
+				byte[] tx_array = Arrays.copyOfRange(temp_ledBuff, start_index, end_index);
+				ret_val =  spi.write(tx_array, size);
+				System.out.println(ret_val + ", " + size);
+			}
+			
 		}
 		
-		//If we were successful in writing, mark the buffer as written to the LEDs 
-		if(ret_val == 0){
-			newBuffer = false;
-		}
+		//Mark the buffer as written to the LEDs 
+		newBuffer = false;
 
-		return ret_val;
+		return 0;
 	}
 	
 	
@@ -99,12 +125,9 @@ public class DotStarsLEDStrip {
 	 */
 	
 	public void clearColorBuffer(){
-		Arrays.fill(ledBuffer, (byte)0x00);
-		//Write in the start/end buffers
-		for(int i = 0; i < startFrame.length; i++)
-			ledBuffer[i] = startFrame[i];
-		for(int i = 0; i < endFrame.length; i++)
-			ledBuffer[i+endFrameOffset] = endFrame[i];
+		for(int i = 0; i < num_leds; i++){
+			setLEDColor(i,0,0,0);
+		}
 		//Mark the buffer as updated
 		newBuffer = true;
 		//we're done!
@@ -122,9 +145,10 @@ public class DotStarsLEDStrip {
 	
 	public void setLEDColor(int index, double r, double g, double b){
 		
-		ledBuffer[index*bytesPerLED + startFrame.length + blueOffset] = convDoubletoByte(b);
-		ledBuffer[index*bytesPerLED + startFrame.length + greenOffset] = convDoubletoByte(g);
-		ledBuffer[index*bytesPerLED + startFrame.length + redOffset] = convDoubletoByte(r);
+		ledBuffer[index*bytesPerLED + led0Offset + globalOffset] = convDoubletoByte(1);
+		ledBuffer[index*bytesPerLED + led0Offset + blueOffset] = convDoubletoByte(b);
+		ledBuffer[index*bytesPerLED + led0Offset + greenOffset] = convDoubletoByte(g);
+		ledBuffer[index*bytesPerLED + led0Offset + redOffset] = convDoubletoByte(r);
 		//Mark the buffer as updated
 		newBuffer = true;
 		//we're done!
@@ -153,7 +177,28 @@ public class DotStarsLEDStrip {
 	}
 	
 	
-	
+	//Java multithreading magic. Do not touch.
+	//Touching will incour the wrath of Cthulhu, god of java and LED Strips.
+	//May the oceans of 1's and 0's rise to praise him.
+    private class DotStarsTask extends TimerTask 
+    {
+        private DotStarsLEDStrip m_leds;
+
+        public DotStarsTask(DotStarsLEDStrip leds) 
+        {
+            if (leds == null) 
+            {
+                throw new NullPointerException("Given DotStars Controller Class was null");
+            }
+            m_leds = leds;
+        }
+
+        @Override
+        public void run() 
+        {
+        	m_leds.updateColors();
+        }
+    }
 	
 	
 	
