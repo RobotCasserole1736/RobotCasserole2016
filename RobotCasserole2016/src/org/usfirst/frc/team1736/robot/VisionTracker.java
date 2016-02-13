1dc305524fcba4a481b3ad22e06ecfe4e653fe8b
Nick Dunne <imdunne8@gmail.com>
2016-02-13 16:20:39 -0600

    Added some vision tracking code for finding target errors

--- RobotCasserole2016/src/org/usfirst/frc/team1736/robot/VisionTracker.java
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/VisionTracker.java
@@ -1,10 +1,15 @@
 package org.usfirst.frc.team1736.robot;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionTracker {
+	private VisionTarget[]  sortedTargets = new VisionTarget[3];
+	private VisionTarget[]  prevSortedTargets = new VisionTarget[3];
+	private double GOAL_ASPECT_RATIO = 1.4286;
+	private double GOAL_AREA_RATIO = 0.2857;
 
 	NetworkTable grip = NetworkTable.getTable("grip");
 	
@@ -16,12 +21,33 @@ public class VisionTracker {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		for(int i = 0; i < 3; i++)
+		{
+			sortedTargets[i] = null;
+			prevSortedTargets[i] = null;
+		}
 	}
 	
 	public void readTargets()
 	{
-		//Assuming that the tracked target with the least error in expected area is the closest target.
-		//But that assumption requires testing, as Aaron does not trust himself 100%
+		for(int i = 0; i < 3; i++)
+			prevSortedTargets[i] = sortedTargets[i];
+		double[] targetAreas = getTargetAreaArray();
+		double[] targetWidths = getTargetWidthArray();
+		double[] targetHeights = getTargetHeightArray();
+		
+		double[] targetAreaErrors = new double[targetAreas.length];
+		double[] targetAspectErrors = new double[targetAreas.length];
+		
+		for(int i = 0; i < targetAreas.length; i++)
+		{
+			targetAreaErrors[i] = Math.abs(targetAreas[i] / (targetWidths[i] * targetHeights[i]) - GOAL_AREA_RATIO);
+			targetAspectErrors[i] = Math.abs(targetWidths[i] * targetHeights[i] - GOAL_ASPECT_RATIO);
+		}
+		
+		//Need to determine a way to figure out the minimum error across both terms
+		
+		//Put the best target(s) into array
 	}
 	
 	public double[] getTargetWidthArray()
