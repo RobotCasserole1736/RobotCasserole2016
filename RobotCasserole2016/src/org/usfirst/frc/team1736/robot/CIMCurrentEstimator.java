0be8731f4dfa0564c8f6aec494a691cae406a157
Chris Gerth <chrisgerth010592@gmail.com>
2016-02-19 22:35:07 -0600

    WIP tuning of current draw and retract time

--- RobotCasserole2016/src/org/usfirst/frc/team1736/robot/CIMCurrentEstimator.java
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/CIMCurrentEstimator.java
@@ -13,7 +13,7 @@ public class CIMCurrentEstimator {
 	private static final double freewheelCurrent_A = 2.7;
 	
 	//Guessed Constants
-	private static final double motorWiringResistance = 0.160; //total guess to make the numbers work?
+	private static final double motorWiringResistance = 0.051; //total guess to make the numbers work? assumed 0.006 for wires and 0.005 for controller
 	
 	//Derived motor constants
 	private static final double ESR = operatingVoltage/stallCurrent_A + motorWiringResistance;
