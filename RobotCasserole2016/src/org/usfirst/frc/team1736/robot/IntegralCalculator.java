9e5a38dde20a484148f8c7171007595d85517403
Chris Gerth <chrisgerth010592@gmail.com>
2016-05-17 19:27:49 -0500

    WIP - working on new classes for integration and PID

---
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/IntegralCalculator.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team1736.robot;
+
+public class IntegralCalculator {
+	
+	public double calcIntegral(double in){
+		//Stub! code goes here!
+		return 0;
+	}
+	
+	public void resetIntegral(){
+		//Stub! code goes here!
+		return;
+	}
+
+}
