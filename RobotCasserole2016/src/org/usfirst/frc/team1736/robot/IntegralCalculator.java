472232d2aa603d321e1c6abd424c7d424e30365c
Chris Gerth <chrisgerth010592@gmail.com>
2016-05-19 07:55:08 -0500

    Implemented integrator class with simple sample & hold integration. More methods to be added in future.

--- RobotCasserole2016/src/org/usfirst/frc/team1736/robot/IntegralCalculator.java
+++ RobotCasserole2016/src/org/usfirst/frc/team1736/robot/IntegralCalculator.java
@@ -1,15 +1,29 @@
 package org.usfirst.frc.team1736.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class IntegralCalculator {
+	private double prev_time;
+	private double accumulator;
+	
+	IntegralCalculator(){
+		prev_time = Timer.getFPGATimestamp();
+		accumulator = 0;
+	}
 	
 	public double calcIntegral(double in){
-		//Stub! code goes here!
-		return 0;
+		double cur_time = Timer.getFPGATimestamp();
+		/*Integration method - "Sample and hold"*/
+		/*TBD - other methods...*/
+		accumulator += in * (cur_time - prev_time);
+		
+		/*Save values for next loop and return*/
+		prev_time = cur_time;
+		return accumulator;
 	}
 	
 	public void resetIntegral(){
-		//Stub! code goes here!
-		return;
+		accumulator = 0;
 	}
 
 }
